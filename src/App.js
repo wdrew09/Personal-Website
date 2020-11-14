d6f63cc107aa892a180df503c7ac8bdc052aee54
Andrew Wilson <awi279@uky.edu>
2020-11-13 19:09:10 -0500

    finished top

--- src/App.js
+++ src/App.js
@@ -2,57 +2,66 @@ import React, { useState, useEffect } from 'react';
 import mainImage from './Images/tripImage3.JPG'
 import styles from './App.module.css';
 
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const App = props => {
   let showOne = false
   let showTwo = false
   const [textOne, setTextOne] = useState("")
   const [textTwo, setTextTwo] = useState("")
+  const [textThree, setTextThree] = useState("")
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setTextOne("Hi,")
-    }, 1000);
+    }, 800);
+
     setTimeout(() => {
       setTextTwo("  I'm Andrew")
-    }, 2000);
+    }, 1500);
+
+    setTimeout(() => {
+      setTextThree("I'm a University of Kentucky Fall 2020 graduate, I have 18 months of front-end industry experience, and I'm looking to start my career in software engineering")
+    }, 3000);
     return () => clearTimeout(timer);
   }, []);
 
-  // let textOneClasses = classNames({
-  //   'styles.TextOne': true,
-  //   'styles.fadeIn' : textOne.length > 0,
-  //   'styles.fadeOut' : textOne.length == 0,
-  // });
+  const scrollDown = () => {
+    window.scrollTo({
+      top: window.innerHeight,
+      behavior: "smooth"
+    });
+  }
 
 
 
   return (
     <div className="App">
-      <header className="App-header">
         <div className={styles.SectionOne}>
-          {/* <img src={mainImage} className={styles.MainImage} alt="image" resizeMode="cover" /> */}
+          <span className={styles.TopButtons}>
+            <a href="https://www.linkedin.com/in/andrew-wilson-98968a1bb" target="_blank">
+              <button className={styles.TopButtonsButtonShow} style={{ marginLeft: '40px', height: '50px', backgroundColor: 'transparent', borderColor: "transparent" }}><FontAwesomeIcon color="white" icon={faLinkedin} size='3x' /></button>
+            </a>
+            <a href="https://github.com/wdrew09" target="_blank">
+              <button className={styles.TopButtonsButtonShow} style={{ marginLeft: '120px', height: '50px', backgroundColor: 'transparent', borderColor: "transparent" }}><FontAwesomeIcon color="white" icon={faGithub} size='3x' /></button>
+            </a>
+          </span>
           <div className={styles.MainImageText}>
             <span className={textOne.length > 0 ? styles.fadeInOne : styles.fadeOutOne}>{textOne}</span>
-            <span className={textTwo.length > 0 ? styles.fadeInTwo : styles.fadeOutOne}>{textTwo}</span>
+            <span className={textTwo.length > 0 ? styles.fadeInOne : styles.fadeOutOne}>{textTwo}</span>
           </div>
+          <button className={styles.DownButton} style={{ backgroundColor: 'transparent', borderColor: "transparent" }} onClick={() => scrollDown()}><FontAwesomeIcon color="white" icon={faArrowCircleDown} size='3x' /></button>
         </div>
+
         <div className={styles.SectionTwo}>
           <span>hellodfg</span>
         </div>
 
         {/* <img src={"/profileImage.jpg"} className={styles.ProfileImage} alt="image" resizeMode="cover"/> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
   );
 }
