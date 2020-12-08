ad23cb4b84d005a8abc1f8ccede15353eccee2e8
Andrew Wilson <awi279@uky.edu>
2020-12-08 14:24:23 -0500

    another try

--- src/Container/SecondPage.js
+++ src/Container/SecondPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './SecondPage.module.css';
 
 import profileImage from '../Images/profileImage.png'
--- src/Container/ThirdPage.js
+++ src/Container/ThirdPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './ThirdPage.module.css';
 
 import FavoritesCard from '../Components/FavoritesCard';
